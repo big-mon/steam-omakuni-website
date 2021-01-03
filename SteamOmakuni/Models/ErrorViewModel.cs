feba0bd60cfab805d72206bd92d41efe5ade46c3
BIG-MON <mrmon93@gmail.com>
2021-01-03 21:50:39 +0900

    プロジェクト ファイルを追加します。

---
+++ SteamOmakuni/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SteamOmakuni.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
