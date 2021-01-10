31ab901d632ab7a6865b8c17a5aac809e286a23e
BIG-MON <mrmon93@gmail.com>
2021-01-11 03:35:36 +0900

    chore: 不要な処理を削除

--- SteamOmakuni/Controllers/HomeController.cs
+++ SteamOmakuni/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using SteamOmakuni.Models;
 using System.Diagnostics;
 
@@ -7,13 +6,6 @@ namespace SteamOmakuni.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             ViewBag.Apps = DAO.RetrieveApps();
